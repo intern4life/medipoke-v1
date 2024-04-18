7c54bc017e0818824bae8b206a3be5d694706c12
David Duran <david.duran@mgb.ch>
2024-04-18 11:36:04 +0200

    Second project commit. Includes basic project setup

---
+++ src/main/java/ch/fhnw/module/medipokev1/MedipokeV1Application.java
@@ -0,0 +1,32 @@
+package ch.fhnw.module.medipokev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MedipokeV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedipokeV1Application.class, args);
+
+    }
+
+    @GetMapping(value="/")
+    public String getWelcomeString() {
+        return "Hello, welcome to MediPoke!";
+    }
+
+    @GetMapping("/nogreeting")
+    public String getNogreeting() {
+        return "This is not a public greeting";
+    }
+
+    @GetMapping("/admin")
+    public String getAdmin() {
+        return "This is the admin page. Are you an ADMIN?";
+    }
+
+}
---
+++ src/main/java/ch/fhnw/module/medipokev1/SecurityConfig.java
@@ -0,0 +1,70 @@
+package ch.fhnw.module.medipokev1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+
+                /*.securityMatcher("/api/**")
+                .authorizeHttpRequests((authorize) -> authorize
+                        //.requestMatchers("/", "/greeting").permitAll()
+                        .requestMatchers("/nogreeting").hasRole("USER")
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        //.anyRequest().denyAll()
+                        .anyRequest().authenticated()
+                )
+                */
+                //TODO: when logged-in with a certain user, you cannot access pages accessible via another user type. Is this authentication hierarchy ADMIN > USER?
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/", "/greeting").permitAll()
+                        .requestMatchers("/nogreeting").hasRole("USER")
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().denyAll()
+                        //.anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> userDetails = new ArrayList<>();
+        UserDetails userDetails1 = User.withDefaultPasswordEncoder()
+                .username("simpleboy")
+                .password("123")
+                .roles("USER")
+                .build();
+
+        UserDetails userDetails2 = User.withDefaultPasswordEncoder()
+                .username("adminboy")
+                .password("1234")
+                .roles("ADMIN")
+                .build();
+
+        userDetails.add(userDetails1);
+        userDetails.add(userDetails2);
+
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+}
